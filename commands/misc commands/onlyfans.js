5b90b4a3d7f3dc43d218aaa17b6d9ceb2b98f1f4
FlagL <80362537+FlagL@users.noreply.github.com>
2021-10-01 20:24:50 +0100

    delete1

--- commands/misc commands/onlyfans.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "onlyfans",
-    aliases: ["onlyfan", "of"],
-    description: "onlyfans command",
-    execute (client, message, success, failed, args, Discord) {
-        message.channel.send("http://bit.ly/flagsonlyfan");
-    }
-}
